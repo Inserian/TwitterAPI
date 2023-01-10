abfbd0509632fde85aeb8464376f4e6c732031cc
Brenton Carter <99493283+Inserian@users.noreply.github.com>
2023-01-10 08:32:09 -0900

    Create TAPI.py

---
+++ TAPI.py
@@ -0,0 +1,16 @@
+import tweepy
+
+# Set up your API credentials
+consumer_key = "your_consumer_key"
+consumer_secret = "your_consumer_secret"
+access_token = "your_access_token"
+access_token_secret = "your_access_token_secret"
+
+# Authenticate with Twitter using your API credentials
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Send a tweet
+tweet = "Hello, World!"
+api.update_status(status=tweet)
